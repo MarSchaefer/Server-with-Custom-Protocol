09957f6727c070dff289ce72a807409b4a4d9f50
MarSchaefer <markusschaefer94@googlemail.com>
2024-03-17 14:27:12 +0100

    struct angepasst

--- Server/Server/Server/ProtocolLayer/DataStructs/RsaPublicKeyExchangeData.cs
+++ Server/Server/Server/ProtocolLayer/DataStructs/RsaPublicKeyExchangeData.cs
@@ -10,5 +10,7 @@ namespace Server.ProtocolLayer.DataStructs
     [StructLayout(LayoutKind.Sequential, Pack = 1, CharSet = CharSet.Ansi)]
     public struct RsaPublicKeyExchangeData
     {
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 296)] // bsp.: 296 für 256byte key, 550 für 512byte key
+        public byte[] publicKey;
     }
 }
